0bbd9cc3541963653f04b0639500b4b814e5baf1
marcdoesdat <89348381+marcdoesdat@users.noreply.github.com>
2026-03-29 14:44:17 -0400

    ajout sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // L'URL est indispensable pour générer les liens du sitemap
+  site: 'https://stephanieweyman.ca',
+  
+  integrations: [
+    sitemap()
+  ],
+});
